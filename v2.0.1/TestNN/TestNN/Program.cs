7f33f697d3de1fff6d591e1708ece9d2b88e9841
Pavel <pavelpontaplev@gmail.com>
2019-10-20 12:25:34 +0300

    auth

--- v2.0.1/TestNN/TestNN/Program.cs
+++ v2.0.1/TestNN/TestNN/Program.cs
@@ -14,8 +14,26 @@ namespace TestNN
     {
         static void Main(string[] args)
         {
+            if (args == null)
+            {
+                Testing();
+            }
+            else
+            {
+                if (args.Length == 1)
+                {
+                    StartWithPath(args[0]);
+                }
+                if (args.Length > 3)
+                {
+                    NewNN(args);
+                }
+            }
+        }
+        static void Testing()
+        {
             int[] array = { 2, 8, 4 };
-            
+
             string str = File.ReadAllText("TD1.txt");
             double[][] vs1 = JsonConvert.DeserializeObject<double[][]>(str);
             //NNModel model = new NNModel(array);   
@@ -33,8 +51,48 @@ namespace TestNN
                     Console.WriteLine(x.Value + "  ");
                 });
                 Console.WriteLine();
-            }            
+            }
             Console.ReadKey();
+        }
+        static void StartWithPath(string path)
+        {
+            if (File.Exists(path))
+            {
+                //NNModel model = NNModel.Open(path);
+            }
+        }
+        static void NewNN(string[] args)
+        {
+            int[] array = new int[args.Length - 1];
+            for (int i = 1; i < args.Length; i++)
+            {
+                if (Int32.TryParse(args[i], out int res))
+                {
+                    array[i] = res;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            string str = File.ReadAllText(args[0]);
+            double[][] vs1 = JsonConvert.DeserializeObject<double[][]>(str);
+
+            NNModel model = new NNModel(array);
+            BackPropagation backPropagation = new BackPropagation();
+            backPropagation.Learn(model, vs1);
+
+            for (int i = 0; i < vs1.Length; i += 2)
+            {
+                var inp = vs1[i];
+                model.Compute(inp).Neurons.ForEach(x =>
+                {
+                    Console.WriteLine(x.Value + "  ");
+                });
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
         }
     }
 }
